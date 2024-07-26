beca85f8c7094a78ad2d8518f2ce2a34f75433a5
adriaan2 <50466901+adriaan2@users.noreply.github.com>
2024-07-26 19:49:49 +0200

    wete

--- obj/Debug/net8.0-windows/chess.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/chess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b3e52c3ae4e9ec41f063dbfa1faf7349c119c7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2386ad3d34ee8b0850138871f7a0bf2d3d63b47d")]
 [assembly: System.Reflection.AssemblyProductAttribute("chess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
